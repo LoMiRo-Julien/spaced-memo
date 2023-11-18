32084e14643ae9f0bfdb57eb07ca152967b78726
Nicolas pierre <pierre.mjenr@gmail.com>
2023-11-18 12:17:20 +0100

    Get-started from scratch
    Maintiens de la structure, retrait de toute la logique sauf certains composant ou fonctions

--- src/components/dialog/CustomDialog.jsx
+++ src/components/dialog/CustomDialog.jsx
@@ -3,43 +3,7 @@ import { Dialog } from "primereact/dialog";
 import React from "react";
 
 const CustomDialog = ({ data, visible, actions }) => {
-  const footerContent = (
-    <div>
-      <h4>C'est ce à quoi tu penses ?</h4>
-      <Button
-        label="Non"
-        icon="pi pi-times"
-        onClick={rejectButton}
-        className="p-button-text"
-      />
-      <Button label="OuI" icon="pi pi-check" onClick={acceptButton} autoFocus />
-    </div>
-  );
-
-  function rejectButton() {
-    actions.update_positionCard(data, "down");
-    actions.hide();
-    actions.show("Niveau abaisser", "error");
-  }
-  function acceptButton() {
-    actions.update_positionCard(data, "up");
-    actions.hide();
-    actions.show("Niveau augmenté", "success");
-  }
-
-  return (
-    <>
-      <Dialog
-        header={data?.title}
-        visible={visible}
-        onHide={() => actions.hide()}
-        footer={footerContent}
-        style={{ width: "70vw" }}
-      >
-        <p> Réponse : {data?.response}</p>
-      </Dialog>
-    </>
-  );
+  return <></>;
 };
 
 export default CustomDialog;
--- src/components/forcedView/ForcedViewSelector.jsx
+++ src/components/forcedView/ForcedViewSelector.jsx
@@ -1,32 +1,7 @@
 import { MultiSelect } from "primereact/multiselect";
 
 const ForcedViewSelector = ({ forcedView, actions }) => {
-  const cities = [
-    { position: 1, niveau: "Niveau 1" },
-    { position: 2, niveau: "Niveau 2" },
-    { position: 3, niveau: "Niveau 3" },
-    { position: 4, niveau: "Niveau 4" },
-    { position: 5, niveau: "Niveau 5" },
-    { position: 6, niveau: "Niveau 6" },
-    { position: 7, niveau: "Niveau 7" },
-  ];
-
-  return (
-    <div className="selector">
-      <label htmlFor="forced_selector">Vue forcé des niveaux</label>
-      <MultiSelect
-        id="forced_selector"
-        value={forcedView}
-        onChange={(e) => actions.update_forcedView(e.value)}
-        options={cities}
-        optionLabel="niveau"
-        selectAllLabel="Tous"
-        display="chip"
-        placeholder="Forcer l'affichage par niveau"
-        maxSelectedLabels={8}
-      />
-    </div>
-  );
+  return <div className="selector"></div>;
 };
 
 export default ForcedViewSelector;
