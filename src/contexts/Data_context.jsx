9221ea57c18cc6939b852912f6669a6b6e58e1fb
Nicolas pierre <pierre.mjenr@gmail.com>
2024-01-30 13:15:54 +0100

    Update Data_context.jsx

--- src/contexts/Data_context.jsx
+++ src/contexts/Data_context.jsx
@@ -1,16 +1,17 @@
 //here localStorage gestion and méthods
 
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const Data_Context = createContext();
 export const Data_Provider = (props) => {
   const test = "ceci est un test";
-
-  // functionnal methods
-  function getData() {}
-  function setData() {}
+  const [data, setData] = useState(
+    localStorage.getItem("data") ? JSON.parse(localStorage.getItem("data")) : []
+  );
 
   return (
-    <Data_Context.Provider value={{}}>{props.children}</Data_Context.Provider>
+    <Data_Context.Provider value={{ data, setData }}>
+      {props.children}
+    </Data_Context.Provider>
   );
 };
